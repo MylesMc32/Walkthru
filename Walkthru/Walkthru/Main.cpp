89c50ebf9d63b0952dfaced92d8da4da09681106
ncc <ncc>
2017-02-22 09:33:49 -0800

    Student Class Done

--- Walkthru/Walkthru/Main.cpp
+++ Walkthru/Walkthru/Main.cpp
@@ -1,11 +1,21 @@
+#include "Student.h"
+
+#include <iostream>
+
+
+
+using namespace std;
+
 int main()
 {
 
 
 
-
-
-
+	Student my = Student("my");
+	cout << my.Getname() << endl;
+	
+	
+	getchar;
 
 	return 0;
 }
